41e3d3af55068447008541534895889987a60fc5
patil <ajayrpatil96k@gmail.com>
2026-07-04 21:13:42 +0530

    chore: Configure Supabase OAuth client credentials in settings and env files

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     jwt_secret: str
     google_client_id: str = "849754791910-kvubjul5bnqi8un3c38on96bdengsn37.apps.googleusercontent.com"
     resend_api_key: str
+    supabase_client_id: str = "14e38149-5272-429d-8391-579ac67918fd"
+    supabase_client_secret: str = "y3Mn3RfqC07U5IWMX4tJHcBe2JR-i5zW9R0oCYVZQTI"
     super_admin_email: str = "team.lcoding@gmail.com"
     super_admin_password: str = "Admin@123"
     port: int = 8001
